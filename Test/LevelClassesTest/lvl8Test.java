f08942ce025021baa622abb3c89b79d6da7c407e
Astrid Rosen <astrid.flisan@gmail.com>
2024-01-07 11:59:17 +0100

    lvl 8 test

--- Test/LevelClassesTest/lvl8Test.java
+++ Test/LevelClassesTest/lvl8Test.java
@@ -1,4 +1,23 @@
 package LevelClassesTest;
 
+import LevelClasses.lvl7;
+import LevelClasses.lvl8;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
 public class lvl8Test {
+    lvl8 testlvl = new lvl8(true);
+    @Test
+    public void checkRequirementsTest () {
+        String inputTest1 = "testar";
+        boolean expectedResult1 = false;
+        boolean actualResult1 = testlvl.checkRequirements(inputTest1);
+        Assertions.assertEquals(expectedResult1, actualResult1);
+
+
+        String inputTest2 = "testarU+1F61D";
+        boolean expectedResult2 = true;
+        boolean actualResult2 = testlvl.checkRequirements(inputTest2);
+        Assertions.assertEquals(expectedResult2, actualResult2);
+    }
 }
