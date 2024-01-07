3112b63c5749eedb344fe74567bbbfc14e9032a3
Astrid Rosen <astrid.flisan@gmail.com>
2024-01-07 12:18:14 +0100

    test lvl10

--- Test/LevelClassesTest/lvl10Test.java
+++ Test/LevelClassesTest/lvl10Test.java
@@ -1,4 +1,26 @@
 package LevelClassesTest;
 
+import LevelClasses.lvl10;
+import LevelClasses.lvl9;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
 public class lvl10Test {
+
+    lvl10 testlvl = new lvl10();
+
+    @Test
+    public void checkRequirementsTest() {
+        String inputTest1 = "testargul";
+        boolean expectedResult1 = false;
+        boolean actualResult1 = testlvl.checkRequirements(inputTest1);
+        Assertions.assertEquals(expectedResult1, actualResult1);
+
+
+        String inputTest2 = "testargrön";
+        boolean expectedResult2 = true;
+        boolean actualResult2 = testlvl.checkRequirements(inputTest2);
+        Assertions.assertEquals(expectedResult2, actualResult2);
+
+    }
 }
