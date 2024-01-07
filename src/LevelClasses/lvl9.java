45ca9a94e85360362f3f9941703109519da0ae43
Astrid Rosen <astrid.flisan@gmail.com>
2024-01-07 12:16:38 +0100

    byggde lvl9

--- src/LevelClasses/lvl9.java
+++ src/LevelClasses/lvl9.java
@@ -1,4 +1,26 @@
 package LevelClasses;
 
-public class lvl9 {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class lvl9 extends Levels{
+    File colors = new File("src/LevelClasses/Datafiles/Colors.txt");
+    Scanner scanFile;
+
+    @Override
+    public boolean checkRequirements(String input) {
+        try {
+            scanFile = new Scanner(colors);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        while (scanFile.hasNext()) {
+            String s = scanFile.nextLine();
+            if (input.toLowerCase().contains(s.toLowerCase())) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
