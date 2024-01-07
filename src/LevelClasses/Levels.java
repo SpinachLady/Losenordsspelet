69e5b2955450e69e2485be7f49eaacd913597cbb
Astrid Rosen <astrid.flisan@gmail.com>
2024-01-07 11:59:47 +0100

    byggde lvl 8

--- src/LevelClasses/Levels.java
+++ src/LevelClasses/Levels.java
@@ -1,6 +1,8 @@
 package LevelClasses;
 
 public abstract class Levels {
+
+    public String instruction = "Ditt lösenord måste innehålla ";
     public abstract boolean checkRequirements(String input);
 
 }
--- src/LevelClasses/lvl8.java
+++ src/LevelClasses/lvl8.java
@@ -1,4 +1,46 @@
 package LevelClasses;
 
-public class lvl8 {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class lvl8 extends Levels{
+    File unicodeSymbols = new File("src/LevelClasses/Datafiles/Characters.txt");
+    Scanner scanFile;
+    String randomLine;
+    String symbol;
+    String unicode;
+
+    public lvl8 (boolean isTest) {
+        ArrayList<String> lines = new ArrayList<>();
+        try {
+            scanFile = new Scanner(unicodeSymbols);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        while (scanFile.hasNext()) {
+            String s = scanFile.nextLine();
+            lines.add(s);
+        }
+        if (isTest) {
+            randomLine = "😝:U+1F61D";
+        }
+        else {
+            int random = (int) (Math.random() * lines.size());
+            randomLine = lines.get(random);
+        }
+        symbol = randomLine.substring(0, randomLine.indexOf(":"));
+        unicode = randomLine.substring(randomLine.indexOf(":") + 1);
+    }
+
+    @Override
+    public boolean checkRequirements(String input) {
+        if (input.contains(unicode)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 }
